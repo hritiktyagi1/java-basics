c6f4368aa57436cdcc1d0ed5f8c9164a58553cad
Hritik Tyagi <108487611+hritiktyagi1@users.noreply.github.com>
2022-11-10 20:05:50 +0530

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,34 @@
+package Recursion;
+/*
+------------
+*
+**
+***
+****
+*****
+-------------------
+ */
+
+public class StarPattern {
+    static void print(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print("*");
+        print(n-1);
+
+    }
+    static void pattern(int i,int j){
+        if(i==0){
+            return;
+        }
+        print(j);
+        System.out.println();
+        pattern(i-1,j+1);
+
+    }
+    public static void main(String[] args) {
+        pattern(5,1);
+
+    }
+}
